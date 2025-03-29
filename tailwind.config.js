2e884a1cb7cfad2b281bbaf336a04e672bccf9b6
Ridhwan Hussain <ridhwanplayzzz@gmail.com>
2025-03-29 13:48:51 +0000

    Create ShadCN

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,15 +8,25 @@ const config = {
     "./node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
-    extend: {
-      fontFamily: {
-        sans: ["var(--font-sans)"],
-        mono: ["var(--font-mono)"],
-      },
-    },
+  	extend: {
+  		fontFamily: {
+  			sans: [
+  				'var(--font-sans)'
+  			],
+  			mono: [
+  				'var(--font-mono)'
+  			]
+  		},
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		},
+  		colors: {}
+  	}
   },
-  darkMode: "class",
-  plugins: [heroui()],
+  darkMode: ["class", 'class'],
+  plugins: [heroui(), require("tailwindcss-animate")],
 }
 
 module.exports = config;
