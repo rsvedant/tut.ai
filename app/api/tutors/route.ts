602eb333ff78ab6fcb8e9afae72449c57915b8c9
Ridhwan Hussain <ridhwanplayzzz@gmail.com>
2025-03-30 22:44:01 +0100

    ADd ID to tutors

--- app/api/tutors/route.ts
+++ app/api/tutors/route.ts
@@ -7,13 +7,14 @@ const models = [
             "A tutor that can help you with maths problems and concepts.",
         systemPrompt:
             "You're a PHD professor in Mathematics. You help students with anything that they have, and your main goal is to make sure that the user understands. Talk in a relax laid back tone.",
+        id: "maths_tutor",
     },
 ];
 
 export const GET = async (req: NextRequest): Promise<NextResponse> => {
     return NextResponse.json(
         models.map((m) => {
-            return { name: m.name, description: m.description };
+            return { id: m.id, name: m.name, description: m.description };
         }),
         {
             status: 200,
