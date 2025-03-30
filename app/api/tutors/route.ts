b24c44e7082d0a97f09f2775a1efba3e7c3dded7
Ridhwan Hussain <ridhwanplayzzz@gmail.com>
2025-03-30 21:39:04 +0100

    Add base for fetching tutors

--- app/api/tutors/route.ts
+++ app/api/tutors/route.ts
@@ -5,13 +5,17 @@ const models = [
         name: "Maths Tutor",
         description:
             "A tutor that can help you with maths problems and concepts.",
+        systemPrompt: "",
     },
 ];
 
 export const GET = async (req: NextRequest): Promise<NextResponse> => {
-    return NextResponse.json(models, {
-        status: 200,
-    });
+    return NextResponse.json(
+        models.map((m) => {
+            return { name: m.name, description: m.description };
+        }),
+        {
+            status: 200,
+        },
+    );
 };
-
-// 
