9ed053ad50f02ba802e94aea2b876cc2016cd6f6
Ridhwan Hussain <ridhwanplayzzz@gmail.com>
2025-03-31 16:59:47 +0100

    Add function for getting session serverside

--- components/auth/auth-provider.tsx
+++ components/auth/auth-provider.tsx
@@ -1,13 +1,11 @@
-"use client";
-
-// displays auth provider in the layout
-
 import { SessionProvider } from "next-auth/react";
 
 type Props = {
     children: React.ReactNode;
 };
 
-export default function AuthProvider({ children }: Props) {
+export default function AuthProvider(props: Props) {
+    const { children } = props;
+    console.log("Props", props);
     return <SessionProvider>{children}</SessionProvider>;
 }
